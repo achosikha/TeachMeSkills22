ae741310950c457ea3c0242ca74841049293c0be
achosikha <40418629+achosikha@users.noreply.github.com>
2023-05-11 23:01:46 +0400

    updates

--- src/JavaIntro.java
+++ src/JavaIntro.java
@@ -1,5 +1,21 @@
-public class JavaIntro {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+// The first Java Lesson
+
+public class JavaIntro
+{
+    public static void main(String[] args)
+    {
+        // Integrated output class
+        System.out.println("Welcome to Java Profession Lesson."); // ln -> next line
+
+        // print
+        System.out.print("Welcome to Java Profession Lesson with print"); // выведи мне сообщение и также останься
+
+        // print
+        System.out.print("Welcome to Java.");
+        System.out.print("This is the next line...");
+
+        // println vs. print
+        System.out.println(); // любой вывод требует чего на выход - передает знак следующей строчки
+        System.out.print(""); // error
     }
 }
