3381d97986af52a6d8d3f535323d0e5c77f16d19
achosikha <40418629+achosikha@users.noreply.github.com>
2023-06-22 21:07:54 +0400

    updates

--- src/ObjectClone/Car.java
+++ src/ObjectClone/Car.java
@@ -1,4 +1,42 @@
 package ObjectClone;
 
-public class Car {
+import java.util.Arrays;
+
+public class Car implements Cloneable {
+    private String model;
+    private String color;
+    private String[] sparParts;
+
+    public Car(String model, String color, String[] sparParts)
+    {
+        this.model = model;
+        this.color = color;
+        this.sparParts = sparParts;
+    }
+
+    public Car(Car clone)
+    {
+        this.model = clone.model;
+        this.color = clone.color;
+        // this.sparParts = clone.sparParts;
+        this.sparParts = Arrays.copyOf(clone.sparParts, clone.sparParts.length);
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setSparParts(String[] sparParts) {
+        this.sparParts = sparParts;
+    }
+
+    @Override
+    public String toString() {
+        return this.model + ". " + this.color + ". " + Arrays.toString(sparParts) + ".";
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
 }
--- src/ObjectClone/Main.java
+++ src/ObjectClone/Main.java
@@ -1,4 +1,41 @@
 package ObjectClone;
 
 public class Main {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Car bmw = new Car("BMW", "Grey", new String[]{"Wheels", "Windows", "Bumper"});
+        Car mercedes = new Car("Mercedes", "Red", new String[]{"Iron", "Wheel", "Some Staff"});
+
+        // opel -> bwm
+        Car opel = bmw;
+
+        System.out.println(opel);
+        opel.setModel("Opel");
+        System.out.println(bmw);
+
+        System.out.println();
+
+        Car honda = new Car(mercedes);
+
+        honda.setModel("Honda");
+        System.out.println(mercedes);
+        System.out.println(honda);
+        System.out.println("Hash Code: " + mercedes.hashCode());
+        System.out.println("Hash Code: " + honda.hashCode());
+
+        honda.setSparParts(new String[]{"Honda", "Mercedes..."});
+        System.out.println(mercedes);
+
+        Car hb = (Car) mercedes.clone();
+        System.out.println(hb);
+
+        String name = "Archil Sikharulidze";
+
+        // hb.hashCode(); -> long (new -> always unique hash code)
+        int x = 10;
+        Integer y =  30;
+        Integer z = 40;
+
+        System.out.println(y.hashCode());
+        System.out.println(z.hashCode());
+    }
 }
