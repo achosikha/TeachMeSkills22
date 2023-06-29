d2993fdc23daac5e7a3055e74d5e9de502b23256
achosikha <40418629+achosikha@users.noreply.github.com>
2023-06-29 21:06:39 +0400

    updates

--- src/ExceptionsPrivate/IncorrectPasswordException.java
+++
@@ -1,9 +0,0 @@
-package ExceptionsPrivate;
-
-public class IncorrectPasswordException extends Exception{
-    public IncorrectPasswordException(String msg)
-    {
-        super(msg);
-        System.out.println(msg);
-    }
-}
---
+++ src/FileClass/MainFile.java
@@ -0,0 +1,4 @@
+package FileClass;
+
+public class MainFile {
+}
